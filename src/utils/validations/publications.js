9d7579b1c3fffa561798438faea0ef9f009b84d0
Ignacio Cunial <igsecu@hotmail.com>
2023-09-19 18:22:04 -0300

    Created publications validations file -> created validateTitle() function

---
+++ src/utils/validations/publications.js
@@ -0,0 +1,10 @@
+// Validates title
+const validateTitle = (title) => {
+  if (!title) return "Title parameter is missing";
+  if (typeof title !== "string") return "Title must be a string";
+  return false;
+};
+
+module.exports = {
+  validateTitle,
+};
