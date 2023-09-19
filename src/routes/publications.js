f1e857e08b396a3ffdc7c3b1ec61f94ea2a9a1e7
Ignacio Cunial <igsecu@hotmail.com>
2023-09-19 18:36:03 -0300

    Created POST / route -> added validations

--- src/routes/publications.js
+++ src/routes/publications.js
@@ -1,4 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const validations = require("../utils/validations/publications");
+
+// Create new publication
+router.post("/", async (req, res, next) => {
+  const { title, price, amount, image, description } = req.body;
+
+  // Validations
+  if (validations.validateTitle(title)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateTitle(title),
+    });
+  }
+
+  if (validations.validatePrice(price)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validatePrice(price),
+    });
+  }
+
+  if (validations.validateAmount(amount)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateAmount(amount),
+    });
+  }
+
+  if (validations.validateDescription(description)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateDescription(description),
+    });
+  }
+});
+
 module.exports = router;
