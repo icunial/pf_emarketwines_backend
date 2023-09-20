9b85eb3757529f0c56768466a953bca8619e3af0
Ignacio Cunial <igsecu@hotmail.com>
2023-09-20 17:14:19 -0300

    Created getPublicationId() function -> get publication by its ID

--- src/controllers/publications.js
+++ src/controllers/publications.js
@@ -11,16 +11,18 @@ const getPublications = async () => {
       },
     });
 
-    dbResults.forEach((r) => {
-      results.push({
-        id: r.id,
-        title: r.title,
-        price: r.price,
-        amount: r.amount,
-        image: r.image,
-        description: r.description,
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          title: r.title,
+          price: r.price,
+          amount: r.amount,
+          image: r.image,
+          description: r.description,
+        });
       });
-    });
+    }
 
     return results;
   } catch (error) {
@@ -28,6 +30,32 @@ const getPublications = async () => {
   }
 };
 
+// Get publication by Id
+const getPublicationById = async (id) => {
+  const result = [];
+
+  try {
+    const dbResult = await Publication.findByPk(id);
+
+    if (dbResult) {
+      result.push({
+        id: dbResult.id,
+        title: dbResult.title,
+        price: dbResult.price,
+        amount: dbResult.amount,
+        image: dbResult.image,
+        description: dbResult.description,
+      });
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to get a publication by its ID");
+  }
+};
+
 module.exports = {
   getPublications,
+  getPublicationById,
 };
