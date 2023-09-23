aa9216552d7f9321e36b56d3fdf4e9796917e14d
Ignacio Cunial <igsecu@hotmail.com>
2023-09-23 20:41:09 -0300

    Created products validations file -> added validateName() function

---
+++ src/utils/validations/products.js
@@ -0,0 +1,10 @@
+// Validates name
+const validateName = (name) => {
+  if (!name) return "Name parameter is missing";
+  if (typeof name !== "string") return "Name must be a string";
+  return false;
+};
+
+module.exports = {
+  validateName,
+};
