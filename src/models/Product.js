f02470ed1079a7f9568c728e3981751fa48bcd38
Ignacio Cunial <igsecu@hotmail.com>
2023-09-23 20:23:30 -0300

    Created Product database model

---
+++ src/models/Product.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Product = db.define("product", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  type: {
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  varietal: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  origin: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.TEXT,
+    defaultValue: null,
+  },
+  cellar: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
