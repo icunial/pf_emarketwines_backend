a66cd67dbeec245eec3c4c2421da30aba1403b9b
Ignacio Cunial <igsecu@hotmail.com>
2023-09-23 20:26:37 -0300

    Updated index.js -> added products router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const publicationsRouter = require("./publications");
+const productsRouter = requiere("./products.js");
 
 // Specify routers root routes
 router.use("/publications", publicationsRouter);
+router.use("/products", productsRouter);
 
 module.exports = router;
