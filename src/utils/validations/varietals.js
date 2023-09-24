98fb153f896c7c0ea690e1b15e22264bb3e37722
Ignacio Cunial <igsecu@hotmail.com>
2023-09-24 20:27:21 -0300

    Updated varietals.js -> added validateDescription() function

--- src/utils/validations/varietals.js
+++ src/utils/validations/varietals.js
@@ -4,3 +4,15 @@ const validateName = (name) => {
   if (typeof name !== "string") return "Name must be a string";
   return false;
 };
+
+// Validates Description
+const validateDescription = (description) => {
+  if (!description) return "Description parameter is missing";
+  if (typeof description !== "string") return "Description must be a string";
+  return false;
+};
+
+module.exports = {
+  validateName,
+  validateDescription,
+};
