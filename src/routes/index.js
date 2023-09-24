73b24cdf1281a61861a2c6857782de31dcf020bf
Ignacio Cunial <igsecu@hotmail.com>
2023-09-24 20:23:08 -0300

    Updated index.js file -> added varietals router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const publicationsRouter = require("./publications");
 const productsRouter = require("./products.js");
+const varietalsRouter = require("./varietals");
 
 // Specify routers root routes
 router.use("/publications", publicationsRouter);
 router.use("/products", productsRouter);
+router.use("/varietals", varietalsRouter);
 
 module.exports = router;
