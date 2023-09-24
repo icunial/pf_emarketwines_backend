fd774346342b08edcb402d2afb13370b2ea0df4d
Ignacio Cunial <igsecu@hotmail.com>
2023-09-24 20:29:39 -0300

    Updated POST / route -> added validations

--- src/routes/varietals.js
+++ src/routes/varietals.js
@@ -3,11 +3,27 @@ const router = express.Router();
 
 const Varietal = require("../models/Varietal");
 
+const validations = require("../utils/validations/varietals");
 const { validateId } = require("../utils/validations/index");
 
 // Create New Varietal
 router.post("/", async (req, res, next) => {
   const { name, description } = req.body;
+
+  // Validations
+  if (validations.validateName(name)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateName(name),
+    });
+  }
+
+  if (validations.validateDescription(description)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateDescription(description),
+    });
+  }
 });
 
 module.exports = router;
