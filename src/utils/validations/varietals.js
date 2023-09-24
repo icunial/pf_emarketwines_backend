c77f7d791807099398d7093bbb10250b39ad6710
Ignacio Cunial <igsecu@hotmail.com>
2023-09-24 20:25:55 -0300

    Created varietals validatation file -> varietals.js -> added validateName() function

---
+++ src/utils/validations/varietals.js
@@ -0,0 +1,6 @@
+// Validates Name
+const validateName = (name) => {
+  if (!name) return "Name parameter is missing";
+  if (typeof name !== "string") return "Name must be a string";
+  return false;
+};
