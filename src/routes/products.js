b2050790690f4940ce9da90d0633881ca15bca9c
Ignacio Cunial <igsecu@hotmail.com>
2023-09-23 21:07:17 -0300

    Updated POST / route -> added validations

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,9 +3,47 @@ const router = express.Router();
 
 const Product = require("../models/Product");
 
+const validations = require("../utils/validations/products");
+
 // Create New Product
 router.post("/", async (req, res, next) => {
-  const { name, type, varietal, origin, cellar } = req.body;
+  const { name, type, varietal, origin, cellar, image } = req.body;
+
+  // Validations
+  if (validations.validateName(name)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateName(name),
+    });
+  }
+
+  if (validations.validateType(type)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateType(type),
+    });
+  }
+
+  if (validations.validateVarietal(varietal)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateVarietal(varietal),
+    });
+  }
+
+  if (validations.validateOrigin(origin)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateOrigin(origin),
+    });
+  }
+
+  if (validations.validateCellar(cellar)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateCellar(cellar),
+    });
+  }
 });
 
 module.exports = router;
