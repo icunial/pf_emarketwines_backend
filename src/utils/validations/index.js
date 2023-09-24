d2f3c3a5c60406366fa10f2cabbf7d5516c216e5
Ignacio Cunial <igsecu@hotmail.com>
2023-09-23 21:44:57 -0300

    Created index.js validations file -> added validateId() function

---
+++ src/utils/validations/index.js
@@ -0,0 +1,8 @@
+// Regular expression to check if string is a valid UUID
+const regexExp =
+  /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+
+// Validate id
+const validateId = (id) => {
+  return regexExp.test(id);
+};
