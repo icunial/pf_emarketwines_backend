d989c99fa5c1663291d8088d9e4058a21261c1b8
Ignacio Cunial <igsecu@hotmail.com>
2023-09-27 20:48:58 -0300

    Created Message database model

---
+++ src/models/Message.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+
+const Message = db.define("message", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Message;
