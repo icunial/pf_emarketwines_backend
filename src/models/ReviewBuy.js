72ed8029a820deb952afe48991811abaafaed22f
Ignacio Cunial <igsecu@hotmail.com>
2023-09-27 20:58:39 -0300

    Created ReviewBuy database model

---
+++ src/models/ReviewBuy.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+
+const ReviewBuy = db.define("reviewBuy", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  stars: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0.0,
+    allowNull: false,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = ReviewBuy;
