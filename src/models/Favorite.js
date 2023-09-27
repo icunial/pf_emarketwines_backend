c8f9362b2cc1551616580acbcb0ba161b180b803
Ignacio Cunial <igsecu@hotmail.com>
2023-09-27 20:47:05 -0300

    Created Favorite database model

---
+++ src/models/Favorite.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+
+const Favorite = db.define("favorite", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  isBanned: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Favorite;
