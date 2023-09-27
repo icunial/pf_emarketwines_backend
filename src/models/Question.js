ec003f3d0138cf95dacad712cc45d317473624e3
Ignacio Cunial <igsecu@hotmail.com>
2023-09-27 20:52:35 -0300

    Created Question database model

---
+++ src/models/Question.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+
+const Question = db.define("question", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  answer: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Question;
