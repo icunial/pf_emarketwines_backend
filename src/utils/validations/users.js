4f147b0eacdd5c94b893a637effdaf351710b98f
Ignacio Cunial <igsecu@hotmail.com>
2023-09-28 20:46:00 -0300

    Created validatePassword() function

--- src/utils/validations/users.js
+++ src/utils/validations/users.js
@@ -5,6 +5,14 @@ const validateUsername = (username) => {
   return false;
 };
 
+// Validates password
+const validatePassword = (password) => {
+  if (!password) return "Password parameter is missing";
+  if (typeof password !== "string") return "Password must be a string";
+  return false;
+};
+
 module.exports = {
   validateUsername,
+  validatePassword,
 };
