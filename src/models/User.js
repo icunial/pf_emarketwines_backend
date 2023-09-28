018a26bb204c0502b02d417d09048381d11d3a95
Ignacio Cunial <igsecu@hotmail.com>
2023-09-27 21:04:15 -0300

    Created User database model

---
+++ src/models/User.js
@@ -0,0 +1,63 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  buyLevel: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+  region: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  balance: {
+    type: DataTypes.FLOAT,
+    defaultValue: 1000,
+  },
+  image: {
+    type: DataTypes.TEXT,
+    defaultValue: null,
+  },
+  isSommelier: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  isBanned: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = User;
