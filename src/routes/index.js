10b74cf29da3dc865159cd996d3dc7b6ca7f13cb
Ignacio Cunial <igsecu@hotmail.com>
2023-09-28 20:41:10 -0300

    Imported users router -> specified users router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const publicationsRouter = require("./publications");
 const productsRouter = require("./products.js");
 const varietalsRouter = require("./varietals");
+const usersRouter = require("./users");
 
 // Specify routers root routes
 router.use("/publications", publicationsRouter);
 router.use("/products", productsRouter);
 router.use("/varietals", varietalsRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
