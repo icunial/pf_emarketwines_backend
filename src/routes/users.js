840333f011f86e4af24b3a7f78d1769f75d0319c
Ignacio Cunial <igsecu@hotmail.com>
2023-09-28 21:16:47 -0300

    Created POST /register route -> create new user -> added password validations

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,4 +3,19 @@ const router = express.Router();
 
 const User = require("../models/User");
 
+const validations = require("../utils/validations/users");
+
+// Create new user
+router.post("/register", async (req, res, next) => {
+  const { password } = req.body;
+
+  // Validations
+  if (validations.validatePassword(password)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validatePassword(password),
+    });
+  }
+});
+
 module.exports = router;
