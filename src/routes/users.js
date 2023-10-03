bfa072d19d700713e56c6127ed1f46dc663171db
Ignacio Cunial <igsecu@hotmail.com>
2023-10-03 20:53:34 -0300

    Updated POST /register route -> added email validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,13 +7,20 @@ const validations = require("../utils/validations/users");
 
 // Create new user
 router.post("/register", async (req, res, next) => {
-  const { password } = req.body;
+  const { password, email } = req.body;
 
   // Validations
   if (validations.validatePassword(password)) {
     return res.status(400).json({
       statusCode: 400,
       msg: validations.validatePassword(password),
+    });
+  }
+
+  if (validations.validateEmail(validations.validateEmail(email))) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateEmail(email),
     });
   }
 });
