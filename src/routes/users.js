490ca82b29a7fabd476009c5b21e03521414ebe0
Ignacio Cunial <igsecu@hotmail.com>
2023-10-06 20:45:29 -0300

    Updated POST /register route -> checks if email exists

--- src/routes/users.js
+++ src/routes/users.js
@@ -44,6 +44,23 @@ router.post("/register", async (req, res, next) => {
       msg: validations.validatePhone(phone),
     });
   }
+
+  try {
+    const emailExist = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (emailExist) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `Email ${email} exists! Try with another one!`,
+      });
+    }
+  } catch (error) {
+    return next("Error trying to register a new user");
+  }
 });
 
 module.exports = router;
