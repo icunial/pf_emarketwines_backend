88cf8df2001ecabc9e0cf845cb3f674bdb5aa1b9
Ignacio Cunial <igsecu@hotmail.com>
2023-10-08 20:59:11 -0300

    Updated GET /:id route -> modified status code if id is not valid

--- src/routes/varietals.js
+++ src/routes/varietals.js
@@ -39,7 +39,7 @@ router.get("/:id", async (req, res, next) => {
 
   if (!validateId(id)) {
     return res.status(400).json({
-      statusCode: 404,
+      statusCode: 400,
       msg: `ID invalid format!`,
     });
   }
