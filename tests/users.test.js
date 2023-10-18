38ad57161372fde84367c9aabd3324e4794a172e
Ignacio Cunial <igsecu@hotmail.com>
2023-10-18 17:29:11 -0300

    Created POST /users/register route test -> password confirmation must be at least 8 character long

--- tests/users.test.js
+++ tests/users.test.js
@@ -252,4 +252,20 @@ describe("POST /register route", () => {
     expect(response.status).toBe(400);
     expect(response.body.msg).toBe("Password Confirmation must be a string");
   });
+  it("it should return a 400 status code -> password confirmation must be 8 character long", async () => {
+    const user = {
+      email: "user1@email.com",
+      username: "User One",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "1234",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe(
+      "Password Confirmation must be at least 8 character long"
+    );
+  });
 });
