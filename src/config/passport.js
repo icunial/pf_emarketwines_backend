ae0d2c67c03f1da1c5914f43c2eda125b105d5e7
Ignacio Cunial <igsecu@hotmail.com>
2023-10-18 18:06:45 -0300

    Created passport local strategy

---
+++ src/config/passport.js
@@ -0,0 +1,55 @@
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+module.exports = (passport) => {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          // Match email
+          const userFound = await User.findOne({
+            where: {
+              email,
+            },
+          });
+          if (userFound) {
+            // Match password
+            bcrypt.compare(password, userFound.password, (err, isMatch) => {
+              if (err) {
+                return done(err, null);
+              }
+              if (isMatch) {
+                return done(null, userFound);
+              } else {
+                return done(null, false, { msg: `Incorrect password!` });
+              }
+            });
+          } else {
+            return done(null, false, { msg: `Incorrect mail!` });
+          }
+        } catch (error) {
+          return done(error, null);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const userFound = await User.findByPk(id);
+      if (userFound) {
+        done(null, userFound);
+      } else {
+        done(null, { msg: `User not found!` });
+      }
+    } catch (error) {
+      done(error, null);
+    }
+  });
+};
