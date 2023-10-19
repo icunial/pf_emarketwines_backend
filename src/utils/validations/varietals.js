2eb4ad6d2df4cdc2e4ca9ea1925c698e939563cc
Ignacio Cunial <igsecu@hotmail.com>
2023-10-19 20:41:26 -0300

    Updated varietals validations -> removed if parameter is missing

--- src/utils/validations/varietals.js
+++ src/utils/validations/varietals.js
@@ -1,13 +1,11 @@
 // Validates Name
 const validateName = (name) => {
-  if (!name) return "Name parameter is missing";
   if (typeof name !== "string") return "Name must be a string";
   return false;
 };
 
 // Validates Description
 const validateDescription = (description) => {
-  if (!description) return "Description parameter is missing";
   if (typeof description !== "string") return "Description must be a string";
   return false;
 };
