7715752e742a5c2b849a206163545e6f1b9574fe
Ignacio Cunial <igsecu@hotmail.com>
2023-10-18 21:27:22 -0300

    Created varietals test file -> create new user

---
+++ tests/varietals.test.js
@@ -0,0 +1,32 @@
+const request = require("supertest");
+const app = require("../index");
+const db = require("../src/db");
+
+beforeAll(async () => {
+  await db.sync({ force: true });
+});
+
+afterAll((done) => {
+  db.close();
+  done();
+});
+
+let user1_id;
+
+describe("POST /register route -> create new user", () => {
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "user1@email.com",
+      username: "User One",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    user1_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("user1@email.com");
+  });
+});
