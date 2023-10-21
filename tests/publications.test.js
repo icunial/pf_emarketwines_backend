b05a25d8d4189bba9a50b1d241fd1b3d98dc1d82
Ignacio Cunial <igsecu@hotmail.com>
2023-10-21 17:56:19 -0300

    Created POST /varietal route test -> create new varietal success

--- tests/publications.test.js
+++ tests/publications.test.js
@@ -11,3 +11,19 @@ afterAll((done) => {
   db.close();
   done();
 });
+
+let varietal1_id;
+
+describe("POST /varietals route -> Create new varietal success", () => {
+  it("it should return 201 status code -> new varietal created", async () => {
+    const varietal = {
+      name: "Varietal 1",
+      description: "Description Varietal 1",
+    };
+    const response = await request(app).post("/varietals").send(varietal);
+    varietal1_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.name).toBe("Varietal 1");
+    expect(response.body.data.description).toBe("Description Varietal 1");
+  });
+});
