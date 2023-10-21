106ac2816bf40637da316a94daa4f79512246dca
Ignacio Cunial <igsecu@hotmail.com>
2023-10-21 17:57:26 -0300

    Created POST /products route test -> create new product success

--- tests/publications.test.js
+++ tests/publications.test.js
@@ -27,3 +27,21 @@ describe("POST /varietals route -> Create new varietal success", () => {
     expect(response.body.data.description).toBe("Description Varietal 1");
   });
 });
+
+let product1_id;
+
+describe("POST /products route -> create new product success", () => {
+  it("it should return 201 status code -> new product created", async () => {
+    const product = {
+      name: "Product 1",
+      type: "Type 1",
+      varietal: "Varietal 1",
+      origin: "Origin 1",
+      cellar: "Cellar",
+    };
+    const response = await request(app).post("/products").send(product);
+    expect(response.status).toBe(201);
+    expect(response.body.data.name).toBe("Product 1");
+    product1_id = response.body.data.id;
+  });
+});
