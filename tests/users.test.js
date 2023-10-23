8a7d60fe2f8399b9d3d6da8699f866ed8aa58bdf
Ignacio Cunial <igsecu@hotmail.com>
2023-10-23 20:40:11 -0300

    Created PUT /password route test -> password confirmation must be a string

--- tests/users.test.js
+++ tests/users.test.js
@@ -795,4 +795,16 @@ describe("PUT /password route -> update password", () => {
       "Password Confirmation parameter is missing"
     );
   });
+  it("it should return 400 status code -> password confirmation parameter must be a string", async () => {
+    const user = {
+      email: "user1@email.com",
+      password: "Password14!",
+      password2: 1234,
+      newPassword,
+    };
+
+    const response = await request(app).put("/users/password").send(user);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe("Password Confirmation must be a string");
+  });
 });
