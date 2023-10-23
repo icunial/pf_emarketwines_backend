5335aa6b9c111fcb2802cbd3b5bb8f4cde9804e0
Ignacio Cunial <igsecu@hotmail.com>
2023-10-23 20:41:07 -0300

    Created PUT /password route test -> passwords not match

--- tests/users.test.js
+++ tests/users.test.js
@@ -807,4 +807,18 @@ describe("PUT /password route -> update password", () => {
     expect(response.status).toBe(400);
     expect(response.body.msg).toBe("Password Confirmation must be a string");
   });
+  it("it should return 400 status code -> passwords not match", async () => {
+    const user = {
+      email: "user1@email.com",
+      password: "Password14!",
+      password2: "Password14@",
+      newPassword,
+    };
+
+    const response = await request(app).put("/users/password").send(user);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe(
+      "Password and Password Confirmation not match"
+    );
+  });
 });
