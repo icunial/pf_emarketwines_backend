0a59e17340aa21b619b2783fc3c4479c5f58b872
Ignacio Cunial <igsecu@hotmail.com>
2023-10-24 20:29:12 -0300

    Created GET /banned/true route test -> not authorized

--- tests/users.test.js
+++ tests/users.test.js
@@ -1130,3 +1130,11 @@ describe("GET /regions route -> get total users by region", () => {
     expect(response.body).toBe(true);
   });
 });
+
+describe("GET /banned/true route -> get banned users", () => {
+  it("it should return 401 status code -> not authorized", async () => {
+    const response = await request(app).get("/users/sommeliers");
+    expect(response.status).toBe(401);
+    expect(response.body.msg).toBe("You are not authorized! Please login...");
+  });
+});
