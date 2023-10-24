3526f3d6351293d2cac4da55ceae07a004de2437
Ignacio Cunial <igsecu@hotmail.com>
2023-10-24 19:40:40 -0300

    Created getBannedUsers() function -> get banned users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -266,6 +266,44 @@ const getTotalUsersByRegion = async (users) => {
   return regions;
 };
 
+// Get banned users
+const getBannedUsers = async () => {
+  const results = [];
+
+  try {
+    const dbResults = await User.findAll({
+      where: {
+        isBanned: true,
+        email: {
+          [Op.not]: "admin@ewines.com",
+        },
+      },
+    });
+
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          username: r.username,
+          email: r.email,
+          image: r.image,
+          region: r.region,
+          phone: r.phone,
+          buyLevel: r.buyLevel,
+          balance: r.balance,
+          isSommelier: r.isSommelier,
+          isBanned: r.isBanned,
+          isVerified: r.isVerified,
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get all users from DB!");
+  }
+};
+
 module.exports = {
   getUsers,
   getUserById,
@@ -276,4 +314,5 @@ module.exports = {
   updatePassword,
   getSommeliers,
   getTotalUsersByRegion,
+  getBannedUsers,
 };
