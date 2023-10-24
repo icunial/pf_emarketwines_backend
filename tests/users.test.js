264bb52dfe1d9c0d34692e7d7dcba13760ed4160
Ignacio Cunial <igsecu@hotmail.com>
2023-10-23 23:26:55 -0300

    Updated PUT /password route test -> password must be a string -> added cookie

--- tests/users.test.js
+++ tests/users.test.js
@@ -784,13 +784,14 @@ describe("PUT /password route -> update password", () => {
   });
   it("it should return 400 status code -> password must be a string", async () => {
     const user = {
-      email: "user1@email.com",
       password: 1234,
       password2: "Password14!",
-      newPassword,
     };
 
-    const response = await request(app).put("/users/password").send(user);
+    const response = await request(app)
+      .put("/users/password")
+      .send(user)
+      .set("Cookie", cookie);
     expect(response.status).toBe(400);
     expect(response.body.msg).toBe("Password must be a string");
   });
