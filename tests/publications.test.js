bf216295013c146cfb12eb1a3e49ce2a3d3323c7
Ignacio Cunial <igsecu@hotmail.com>
2023-10-25 15:06:44 -0300

    Created POST /users/register route tests -> created a no admin and an admin new user

--- tests/publications.test.js
+++ tests/publications.test.js
@@ -12,6 +12,45 @@ afterAll((done) => {
   done();
 });
 
+let user1_id, admin_id;
+let cookie;
+
+describe("POST /users/register route -> create a no admin new user", () => {
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "user1@email.com",
+      username: "User One",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    user1_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("user1@email.com");
+  });
+});
+
+describe("POST /users/register route -> create an admin new user", () => {
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "admin@ewines.com",
+      username: "Admin",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    admin_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("admin@ewines.com");
+  });
+});
+
 let varietal1_id;
 
 describe("POST /varietals route -> Create new varietal success", () => {
