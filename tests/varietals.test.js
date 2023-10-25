5249cd989341e6a77ea59ed97001519d99b8375d
Ignacio Cunial <igsecu@hotmail.com>
2023-10-25 08:35:24 -0300

    Created PUT /varietals/:id route test -> admin user logged in

--- tests/varietals.test.js
+++ tests/varietals.test.js
@@ -301,6 +301,17 @@ describe("PUT /varietals/:id route -> updated varietal", () => {
     expect(response.status).toBe(401);
     expect(response.body.msg).toBe("You are not authorized! Please login...");
   });
+  it("it should return a 200 status code -> admin user logged in", async () => {
+    const user = {
+      email: "admin@ewines.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
   it("it should return 400 status code -> id invalid format", async () => {
     const varietal = {
       name: "New Name",
