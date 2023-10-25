a9136fa81e7ad0e8fe31dbe8e02ffeac79257500
Ignacio Cunial <igsecu@hotmail.com>
2023-10-24 21:11:09 -0300

    Created DELETE /:id route test -> login with admin user

--- tests/users.test.js
+++ tests/users.test.js
@@ -1220,4 +1220,15 @@ describe("DELETE /:id route -> delete user by id", () => {
     expect(response.status).toBe(401);
     expect(response.body.msg).toBe("You are not authorized! Please login...");
   });
+  it("it should return 200 status code -> login with admin user", async () => {
+    const user = {
+      email: "admin@ewines.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
 });
