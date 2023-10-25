7113452cdd42b9b3624bc931e4c23859ae7443c1
Ignacio Cunial <igsecu@hotmail.com>
2023-10-25 17:09:55 -0300

    Created POST /users/register route test -> create new user success

--- tests/publications.test.js
+++ tests/publications.test.js
@@ -31,6 +31,21 @@ describe("POST /users/register route -> create a no admin new user", () => {
     expect(response.status).toBe(201);
     expect(response.body.data.email).toBe("user1@email.com");
   });
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "user2@email.com",
+      username: "User Two",
+      password: "Password14!",
+      region: "Region Two",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    user1_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("user2@email.com");
+  });
 });
 
 describe("POST /users/register route -> create an admin new user", () => {
