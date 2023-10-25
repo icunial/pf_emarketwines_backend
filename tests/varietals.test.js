441532d61772ffa568b6eb3c856d99cd245df8c7
Ignacio Cunial <igsecu@hotmail.com>
2023-10-25 08:31:24 -0300

    Created POST /varietals -> create new varietals validations -> admin user logged in

--- tests/varietals.test.js
+++ tests/varietals.test.js
@@ -60,6 +60,17 @@ describe("GET /varietals route -> no varietals saved in DB", () => {
 });
 
 describe("POST /varietals route -> Create new varietal validations", () => {
+  it("it should return a 200 status code -> admin user logged in", async () => {
+    const user = {
+      email: "admin@ewines.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
   it("it should return 400 status code -> name parameter is missing", async () => {
     const varietal = {
       description: "Description Varietal 1",
