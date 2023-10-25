7dc7d592ad5d1f5da30b5438d3a47aca6ecf8407
Ignacio Cunial <igsecu@hotmail.com>
2023-10-25 08:39:40 -0300

    Created PUT /varietals/:id route test -> logout process

--- tests/varietals.test.js
+++ tests/varietals.test.js
@@ -418,6 +418,13 @@ describe("PUT /varietals/:id route -> updated varietal", () => {
       "New Description Varietal 1"
     );
   });
+  it("it should return a 200 status code -> logout process", async () => {
+    const response = await request(app)
+      .get("/users/logout")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+  });
 });
 
 describe("DELETE /varietals/:id route -> delete varietal", () => {
