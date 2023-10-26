d292cedea499775900b4f8c54b660d916fb34a5d
Ignacio Cunial <igsecu@hotmail.com>
2023-10-26 09:28:52 -0300

    Created POST /publications route test -> create new publication success

--- tests/favorites.test.js
+++ tests/favorites.test.js
@@ -153,3 +153,135 @@ describe("POST /products route -> create new product success", () => {
     expect(response.body).toBe(true);
   });
 });
+
+let publication1_id,
+  publication2_id,
+  publication3_id,
+  publication4_id,
+  publication5_id;
+
+describe("POST /publications route -> create new publication success", () => {
+  it("it should return a 200 status code -> no admin user logged in", async () => {
+    const user = {
+      email: "user1@email.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
+  it("it should return 201 status code -> new publication success", async () => {
+    const publication = {
+      title: "Publication 1",
+      price: 100,
+      amount: 100,
+      description: "Description Publication 1",
+      product: "Product 1",
+    };
+
+    const response = await request(app)
+      .post("/publications")
+      .send(publication)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(201);
+    expect(response.body.data.title).toBe("Publication 1");
+    publication1_id = response.body.data.id;
+  });
+  it("it should return 201 status code -> new publication success", async () => {
+    const publication = {
+      title: "Publication 2",
+      price: 500,
+      amount: 1000,
+      description: "Description Publication 2",
+      product: "Product 2",
+    };
+
+    const response = await request(app)
+      .post("/publications")
+      .send(publication)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(201);
+    expect(response.body.data.title).toBe("Publication 2");
+    publication2_id = response.body.data.id;
+  });
+  it("it should return a 200 status code -> logout process", async () => {
+    const response = await request(app)
+      .get("/users/logout")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+  });
+  it("it should return a 200 status code -> no admin user logged in", async () => {
+    const user = {
+      email: "user2@email.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
+  it("it should return 201 status code -> new publication success", async () => {
+    const publication = {
+      title: "Publication 3",
+      price: 1500,
+      amount: 700,
+      description: "Description Publication 3",
+      product: "Product 1",
+    };
+
+    const response = await request(app)
+      .post("/publications")
+      .send(publication)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(201);
+    expect(response.body.data.title).toBe("Publication 3");
+    publication3_id = response.body.data.id;
+  });
+  it("it should return 201 status code -> new publication success", async () => {
+    const publication = {
+      title: "Publication 4",
+      price: 2000,
+      amount: 50,
+      description: "Description Publication 4",
+      product: "Product 1",
+    };
+
+    const response = await request(app)
+      .post("/publications")
+      .send(publication)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(201);
+    expect(response.body.data.title).toBe("Publication 4");
+    publication4_id = response.body.data.id;
+  });
+  it("it should return 201 status code -> new publication success", async () => {
+    const publication = {
+      title: "Publication 5",
+      price: 3000,
+      amount: 120,
+      description: "Description Publication 5",
+      product: "Product 2",
+    };
+
+    const response = await request(app)
+      .post("/publications")
+      .send(publication)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(201);
+    expect(response.body.data.title).toBe("Publication 5");
+    publication5_id = response.body.data.id;
+  });
+  it("it should return a 200 status code -> logout process", async () => {
+    const response = await request(app)
+      .get("/users/logout")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+  });
+});
+
+/******************************************************** */
