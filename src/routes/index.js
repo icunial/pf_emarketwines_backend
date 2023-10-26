b3de34a56636018d3ebfd8d65df36d091d1e7e43
Ignacio Cunial <igsecu@hotmail.com>
2023-10-26 09:20:10 -0300

    Imported favorites router and specified favorites router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const publicationsRouter = require("./publications");
 const productsRouter = require("./products.js");
 const varietalsRouter = require("./varietals");
 const usersRouter = require("./users");
+const favoritesRouter = require("./favorites");
 
 // Specify routers root routes
 router.use("/publications", publicationsRouter);
 router.use("/products", productsRouter);
 router.use("/varietals", varietalsRouter);
 router.use("/users", usersRouter);
+router.use("/favorites", favoritesRouter);
 
 module.exports = router;
