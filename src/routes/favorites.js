b003c0fb98196c388972a53535510fa55ae40259
Ignacio Cunial <igsecu@hotmail.com>
2023-10-26 16:59:10 -0300

    Created DELETE /:id route -> delete a favorite of a publication

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -58,7 +58,13 @@ router.get("/:id", async (req, res, next) => {
       });
     }
 
-    console.log(results);
+    if (!results.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        favorites: results.length,
+        data: results,
+      });
+    }
 
     res.status(200).json({
       statusCode: 200,
@@ -118,6 +124,59 @@ router.post("/:id", ensureAuthenticated, async (req, res, next) => {
     }
   } catch (error) {
     return next("Error trying to favorite a publication");
+  }
+});
+
+// Delete a favorite
+router.delete("/:id", ensureAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!validateId(id)) {
+    return res.status(400).json({
+      statusCode: 404,
+      msg: `ID invalid format!`,
+    });
+  }
+
+  try {
+    const publication = await getPublicationById(id);
+
+    if (!publication.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Publication with ID: ${id} not found!`,
+      });
+    }
+
+    const favoriteFound = await Favorite.findOne({
+      where: {
+        publicationId: id,
+        userId: req.user.id,
+      },
+    });
+
+    if (!favoriteFound) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "You can not delete a favorite of a publication you did not favorite!",
+      });
+    }
+
+    const deletedFavorites = await Favorite.destroy({
+      where: {
+        publicationId: id,
+        userId: req.user.id,
+      },
+    });
+
+    if (deletedFavorites === 1) {
+      return res.status(200).json({
+        statusCode: 200,
+        msg: `You removed your favorite of publication ${id}`,
+      });
+    }
+  } catch (error) {
+    return next("Error trying to delete a favorite of a publication");
   }
 });
 
