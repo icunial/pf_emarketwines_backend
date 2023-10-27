16fc39c880201148eccf05fa9737ee2b585dfc8e
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 12:58:54 -0300

    Created POST / route -> create new buy

--- src/routes/buys.js
+++ src/routes/buys.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const Publication = require("../models/Publication");
+const Buy = require("../models/Buy");
+
+const {
+  validateId,
+  ensureAuthenticated,
+} = require("../utils/validations/index");
+
+// Create new buy
+router.post("/", ensureAuthenticated, async (req, res, next) => {
+  const { publicationId, currency, paymentMethod, totalAmount } = req.body;
+
+  // Validations
+});
+
 module.exports = router;
