403a509c7fc4e1a3df8e3f6bd72df17cde67539c
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 13:19:21 -0300

    Created POST /buys route test -> payment method not available

--- tests/buys.test.js
+++ tests/buys.test.js
@@ -382,4 +382,19 @@ describe("POST /buys route -> create new buy validations", () => {
     expect(response.status).toBe(400);
     expect(response.body.msg).toBe("Payment Method must be a string");
   });
+  it("it should return 400 status code -> payment method not available", async () => {
+    const buy = {
+      currency: "ARG",
+      paymentMethod: "DEB",
+      totalAmount: 1000,
+      publicationId: 1,
+    };
+
+    const response = await request(app)
+      .post("/buys")
+      .send(buy)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe("Payment Method not available");
+  });
 });
