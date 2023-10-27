fa1eef61992de8c19a56cc4d5a7f30572e138c3a
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 13:08:03 -0300

    Updated POST / route -> added validations

--- src/routes/buys.js
+++ src/routes/buys.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const Publication = require("../models/Publication");
 const Buy = require("../models/Buy");
 
+const validations = require("../utils/validations/buys");
+
 const {
   validateId,
   ensureAuthenticated,
@@ -14,6 +16,26 @@ router.post("/", ensureAuthenticated, async (req, res, next) => {
   const { publicationId, currency, paymentMethod, totalAmount } = req.body;
 
   // Validations
+  if (validations.validateCurrency(currency)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateCurrency(currency),
+    });
+  }
+
+  if (validations.validatePaymentMethod(paymentMethod)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validatePaymentMethod(paymentMethod),
+    });
+  }
+
+  if (validations.validateTotalAmount(totalAmount)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateTotalAmount(totalAmount),
+    });
+  }
 });
 
 module.exports = router;
