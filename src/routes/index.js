9939e30c7500f81e070ded3def9ba397353f6f46
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 12:51:33 -0300

    Updated index.js file -> added buys router -> specified buys router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const productsRouter = require("./products.js");
 const varietalsRouter = require("./varietals");
 const usersRouter = require("./users");
 const favoritesRouter = require("./favorites");
+const buysRouter = require("./buys");
 
 // Specify routers root routes
 router.use("/publications", publicationsRouter);
@@ -13,5 +14,6 @@ router.use("/products", productsRouter);
 router.use("/varietals", varietalsRouter);
 router.use("/users", usersRouter);
 router.use("/favorites", favoritesRouter);
+router.use("/buys", buysRouter);
 
 module.exports = router;
