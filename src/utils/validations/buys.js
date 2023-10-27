7b841fd7592bd98495995ea0f0930fe7c9b585cf
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 13:03:17 -0300

    Created validatePaymentMethod() function

--- src/utils/validations/buys.js
+++ src/utils/validations/buys.js
@@ -5,3 +5,13 @@ const validateCurrency = (currency) => {
   if (currency !== "ARG" && currency !== "USD") return "Currency not available";
   return false;
 };
+
+// Validates paymentMethod
+const validatesPaymentMethod = (paymentMethod) => {
+  if (!paymentMethod) return "Payment Method parameter is missing";
+  if (typeof paymentMethod !== "string")
+    return "Payment Method must be a string";
+  if (paymentMethod !== "CASH" && paymentMethod !== "CARD")
+    return "Payment Method not available";
+  return false;
+};
