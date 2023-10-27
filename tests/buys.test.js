81df030e90b27b255a5f83cc960771f49f3cdd20
Ignacio Cunial <igsecu@hotmail.com>
2023-10-27 16:37:56 -0300

    Created GET /buys/sales route test -> no admin user logged in

--- tests/buys.test.js
+++ tests/buys.test.js
@@ -864,4 +864,15 @@ describe("GET /buys/sales -> get user sales", () => {
     expect(response.status).toBe(200);
     expect(response.body).toBe(true);
   });
+  it("it should return 200 status code -> no admin user logged in", async () => {
+    const user = {
+      email: "user2@email.com",
+      password: "Password14!",
+    };
+
+    const response = await request(app).post("/users/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+    cookie = response.headers["set-cookie"];
+  });
 });
