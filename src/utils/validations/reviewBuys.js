e1436ab07477b6805cb632412099ddaeadaacb4c
Ignacio Cunial <igsecu@hotmail.com>
2023-10-31 19:52:14 -0300

    Updated validateStars() function -> added between 0 and 5 validation

--- src/utils/validations/reviewBuys.js
+++ src/utils/validations/reviewBuys.js
@@ -1,14 +1,14 @@
 // Validates stars
 const validateStars = (stars) => {
-  if (!validateStars) return "Stars parameter is missing";
-  if (typeof validateStars !== "number") return "Stars must be a number";
-  if (stars < 0 && stars > 5) return "Stars must be between 0 and 5";
+  if (!stars) return "Stars parameter is missing";
+  if (typeof stars !== "number") return "Stars must be a number";
+  if (stars < 0 || stars > 5) return "Stars must be between 0 and 5";
   return false;
 };
 
 // Validates text
 const validateText = (text) => {
-  if (!text) return "Text paratemer is missing";
+  if (!text) return "Text parameter is missing";
   if (typeof text !== "string") return "Text must be a string";
   return false;
 };
