97badd32cfb22a0f737be8634f01ddfd42bb7c36
Ignacio Cunial <igsecu@hotmail.com>
2023-10-31 20:06:53 -0300

    Created validateText() function validation

---
+++ src/utils/validations/reviews.js
@@ -0,0 +1,10 @@
+// Validates text
+const validateText = (text) => {
+  if (!text) return "Text parameter is missing";
+  if (typeof text !== "string") return "Text must be a string";
+  return false;
+};
+
+module.exports = {
+  validateText,
+};
