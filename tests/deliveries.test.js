1f6d910ba77e44af9413b394dc9af8b247492a87
Ignacio Cunial <igsecu@hotmail.com>
2023-11-01 08:55:38 -0300

    Created POST /deliveries route test -> logout process

--- tests/deliveries.test.js
+++ tests/deliveries.test.js
@@ -448,4 +448,11 @@ describe("POST /deliveries route -> create new delivery", () => {
       `You can not deliver a buy that is not yours!`
     );
   });
+  it("it should return 200 status code -> logout process", async () => {
+    const response = await request(app)
+      .get("/users/logout")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
+  });
 });
