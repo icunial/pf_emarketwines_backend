44005fbdacb56ce0495d7daf36f7ccf8512e561f
Ignacio Cunial <igsecu@hotmail.com>
2023-11-01 18:57:22 -0300

    Updated validateText() function -> replaced message with text

---
+++ src/utils/validations/conversations.js
@@ -0,0 +1,10 @@
+// Validates message
+const validateText = (text) => {
+  if (!text) return "Text parameter is missing";
+  if (typeof text !== "string") return "Text must be a string";
+  return false;
+};
+
+module.exports = {
+  validateText,
+};
