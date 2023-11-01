2f030567ace8ec2bb6be1b5a399e2c8aa6b29237
Ignacio Cunial <igsecu@hotmail.com>
2023-10-31 21:15:17 -0300

    Created validateAnswer() function

--- src/utils/validations/questions.js
+++ src/utils/validations/questions.js
@@ -4,3 +4,15 @@ const validateText = (text) => {
   if (typeof text !== "string") return "Text must be a string";
   return false;
 };
+
+// Validates answer
+const validateAnswer = (answer) => {
+  if (!answer) return "Answer parameter is missing";
+  if (typeof answer !== "string") return "Answer must be a string";
+  return false;
+};
+
+module.exports = {
+  validateText,
+  validateAnswer,
+};
