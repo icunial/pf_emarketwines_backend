accf513ba9546981624c1172f135ebb0b1fbc81c
Ignacio Cunial <igsecu@hotmail.com>
2023-11-01 16:14:08 -0300

    Updated validateStatus() function -> added toUpperCase() method to parameter

--- src/utils/validations/deliveries.js
+++ src/utils/validations/deliveries.js
@@ -2,7 +2,11 @@
 const validateStatus = (status) => {
   if (!status) return "Status parameter is missing";
   if (typeof status !== "string") "Status must be a string";
-  if (status !== "PENDING" && status !== "IN TRANSIT" && status !== "ARRIVED") {
+  if (
+    status.toUpperCase() !== "PENDING" &&
+    status.toUpperCase() !== "IN TRANSIT" &&
+    status.toUpperCase() !== "ARRIVED"
+  ) {
     return "Status not available";
   }
   return false;
