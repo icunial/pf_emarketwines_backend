a18693c78e9cf89b504937ae63955cf7bfe024ae
Ignacio Cunial <igsecu@hotmail.com>
2023-11-01 20:26:02 -0300

    Created getConversationById() function

--- src/controllers/conversations.js
+++ src/controllers/conversations.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const Conversation = require("../models/Conversation");
+const Message = require("../models/Message");
 const { Op } = require("sequelize");
 
 // Get conversation by members
@@ -48,6 +49,89 @@ const getConversationByMembers = async (member1, member2) => {
   }
 };
 
+// Get conversations
+const getConversations = async (id) => {
+  const results = [];
+
+  try {
+    const dbResults = await Conversation.findAll({
+      include: {
+        model: User,
+        where: {
+          id,
+        },
+      },
+    });
+
+    if (dbResults) {
+      for (let result of dbResults) {
+        const conversation = await Conversation.findByPk(result.id, {
+          include: {
+            model: User,
+            where: {
+              id: {
+                [Op.not]: id,
+              },
+            },
+          },
+        });
+
+        if (conversation) {
+          results.push({
+            id: conversation.id,
+            username: conversation.users[0].username,
+          });
+        }
+      }
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get all conversations");
+  }
+};
+
+// Get conversation by id
+const getConversationById = async (id, userId) => {
+  const results = [];
+
+  try {
+    const conversation = await Conversation.findByPk(id, {
+      include: {
+        model: User,
+        where: {
+          id: userId,
+        },
+      },
+    });
+
+    if (conversation) {
+      const messages = await Message.findAll({
+        include: {
+          model: User,
+        },
+        where: {
+          conversationId: id,
+        },
+        order: [["createdAt", "ASC"]],
+      });
+      messages.forEach((m) => {
+        results.push({
+          text: m.text,
+          username: m.user.username,
+          date: m.createdAt,
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get a conversation by id");
+  }
+};
+
 module.exports = {
   getConversationByMembers,
+  getConversations,
+  getConversationById,
 };
