240836002c47d923eee83c2bfb2268f33fc51cea
Ignacio Cunial <igsecu@hotmail.com>
2023-11-01 16:18:26 -0300

    Created new users routes test

---
+++ tests/conversations.test.js
@@ -0,0 +1,67 @@
+const request = require("supertest");
+const app = require("../index");
+const db = require("../src/db");
+
+beforeAll(async () => {
+  await db.sync({ force: true });
+  // await Product.truncate()
+});
+
+afterAll((done) => {
+  db.close();
+  done();
+});
+
+let user1_id, user2_id, admin_id;
+let cookie;
+
+describe("POST /users/register route -> create a no admin new user", () => {
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "user1@email.com",
+      username: "User One",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    user1_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("user1@email.com");
+  });
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "user2@email.com",
+      username: "User Two",
+      password: "Password14!",
+      region: "Region Two",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    user2_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("user2@email.com");
+  });
+});
+
+describe("POST /users/register route -> create an admin new user", () => {
+  it("it should return a 201 status code -> create new user successfully", async () => {
+    const user = {
+      email: "admin@ewines.com",
+      username: "Admin",
+      password: "Password14!",
+      region: "Region One",
+      phone: "12345678",
+      password2: "Password14!",
+    };
+
+    const response = await request(app).post("/users/register").send(user);
+    admin_id = response.body.data.id;
+    expect(response.status).toBe(201);
+    expect(response.body.data.email).toBe("admin@ewines.com");
+  });
+});
