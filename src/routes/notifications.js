dcb94f6c2eea689123f42c6cb996ab8bce269d05
Ignacio Cunial <igsecu@hotmail.com>
2023-11-02 12:07:12 -0300

    Created GET / route -> get user notifications

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -6,4 +6,35 @@ const {
   ensureAuthenticated,
 } = require("../utils/validations/index");
 
+const Notification = require("../models/Notification");
+
+// Get user notifications
+router.get("/", ensureAuthenticated, async (req, res, next) => {
+  try {
+    const notifications = await Notification.findAll({
+      attributes: ["text"],
+      where: {
+        userId: req.user.id,
+      },
+      order: [["createdAt", "DESC"]],
+    });
+
+    if (!notifications.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: "You do not have new notifications!",
+      });
+    }
+
+    console.log(notifications);
+
+    res.status(200).json({
+      statusCode: 200,
+      data: notifications,
+    });
+  } catch (error) {
+    return next("Error trying to get all notifications from a user");
+  }
+});
+
 module.exports = router;
